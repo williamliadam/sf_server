5f8d0317f24280e060db24b12acbeecdd6076f2c
willadam <liwenkang4757@outlook.com>
2024-12-21 02:43:46 +0800

    fix docker file

--- src/main.ts
+++ src/main.ts
@@ -21,6 +21,6 @@ async function bootstrap() {
 		.build();
 	const document = SwaggerModule.createDocument(app, config);
 	SwaggerModule.setup('api', app, document);
-	await app.listen(process.env.PORT || 3000, '0.0.0.0');
+	await app.listen(process.env.APP_PORT || 3000, '0.0.0.0');
 }
 bootstrap();
