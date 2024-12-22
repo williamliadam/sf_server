dd4dd19d0631257eb1af0c23cac59cc94c6de0fa
willadam <liwenkang4757@outlook.com>
2024-12-22 22:12:39 +0800

    change compose

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -6,9 +6,9 @@ async function main() {
     where: { email: process.env.ADMIN_EMAIL },
     update: {},
     create: {
-      email: process.env.ADMIN_EMAIL,
+      email: process.env.ADMIN_EMAIL || "",
       name: 'WilliamLi',
-      password: process.env.ADMIN_PASSWORD,
+      password: process.env.ADMIN_PASSWORD || "",
       role: "ADMIN",
       phone: "178555833177",
       myPosts: {
